790cbdc7027edc17d083bd56de97f51df2f6f2b4
Greg Westneat <greg@fosterwebmarketing.com>
2017-07-30 13:56:57 -0700

    removed bootstrap and convertyed to react-native-for-web

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, { Component } from 'react';
-import { Button, Jumbotron, Container, Row, Col } from 'reactstrap';
-
-import './App.css';
-
-import DrawDeck from './deck.js';
+import React, { Component } from "react";
+import Header from "./components/Header/Header";
+import { Button, View } from "react-native";
+import DrawDeck from "./deck.js";
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      celeb: 'Celebrity',
-      disease: 'Health Condition',
+      celeb: "Celebrity",
+      disease: "Health Condition"
     };
 
-    this.celebDeck = new DrawDeck(require('../packs/celebs.js'));
-    this.diseaseDeck = new DrawDeck(require('../packs/diseases.js'));
+    this.celebDeck = new DrawDeck(require("./packs/celebs.js"));
+    this.diseaseDeck = new DrawDeck(require("./packs/diseases.js"));
   }
 
   draw() {
     let newState = {
       celeb: this.celebDeck.draw(),
-      disease: this.diseaseDeck.draw(),
+      disease: this.diseaseDeck.draw()
     };
 
     this.setState(newState);
@@ -30,23 +28,29 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Container className="App">
-          <Row><Col>
-            <Jumbotron>
-              <h2>Celebrity Hospital</h2>
-            </Jumbotron>
-          </Col></Row>
-          <Row><Col>
-            <div className="pairing">
-              {this.state.celeb} with {this.state.disease}
-            </div>
-          </Col></Row>
-          <Row><Col>
-            <div className="draw-button">
-              <Button onClick={this.draw.bind(this)}>Pair!</Button>
-            </div>
-          </Col></Row>
-        </Container>
+        <Header />
+        <View>
+          {this.state.celeb} with {this.state.disease}
+        </View>
+        <Button
+          onPress={this.draw.bind(this)}
+          title="Pair"
+          accessibilityLabel="Click to pair celebrity with disease"
+        />
+        {/*
+          <Row>
+            <Col>
+              <div className="pairing">
+                {this.state.celeb} with {this.state.disease}
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <div className="draw-button">
+              </div>
+            </Col>
+          </Row>*/}
       </div>
     );
   }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const Header = () =>
+  <View>
+    <Text accessibilityRole="heading" aria-level="1">
+      Celebrity Hospital
+    </Text>
+  </View>;
+
+export default Header;
